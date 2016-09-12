18f8252b51347d7d6ec28de7f72eb5ceae5e19a6
SHALTY <shealtiel84@gmail.com>
2016-09-12 13:24:43 +0300

    ticket factory

---
+++ app/src/main/java/com/teamrm/teamrm/Interfaces/productID.java
@@ -0,0 +1,14 @@
+package com.teamrm.teamrm.Interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 12/09/2016.
+ */
+public interface ProductID {
+     String TICKET_ID_A00A = "A00Admin";
+     String TICKET_ID_A00T = "A00Tech";
+
+
+}
