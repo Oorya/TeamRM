77c0cdf920a69eecc8217ddf09848e34a78324ee
Oorya <ooryakarta@gmail.com>
2016-09-12 19:51:48 +0300

    rename statement

--- app/src/main/java/com/teamrm/teamrm/Interfaces/productID.java
+++ app/src/main/java/com/teamrm/teamrm/Interfaces/productID.java
@@ -1,14 +1,11 @@
 package com.teamrm.teamrm.Interfaces;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by Administrator on 12/09/2016.
  */
 public interface ProductID {
-     String TICKET_ID_A00A = "A00Admin";
-     String TICKET_ID_A00T = "A00Tech";
+    String TICKET_ID_A00A = "A00Admin";
+    String TICKET_ID_A00T = "A00Tech";
 
 
 }
