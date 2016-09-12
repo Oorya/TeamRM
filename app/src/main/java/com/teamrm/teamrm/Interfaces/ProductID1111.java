6b9ed0234edc9efe7dae50975387ac522572941e
shalty84 <shealtiel84@gmail.com>
2016-09-13 00:17:23 +0300

    Update ProductID1111.java

--- app/src/main/java/com/teamrm/teamrm/Interfaces/ProductID1111.java
+++ app/src/main/java/com/teamrm/teamrm/Interfaces/ProductID1111.java
@@ -3,7 +3,7 @@ package com.teamrm.teamrm.Interfaces;
 /**
  * Created by shalty on 12/09/2016.
  */
-public interface ProductID1111 {
+public interface ProductID {
     String TICKET_ID_A00A = "A00Admin";
     String TICKET_ID_A00T = "A00Tech";
 
