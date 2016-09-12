74b3768b5b131f30e4154b36b53d2d8b9066d299
Shalty <shealtiel84@gmail.com>
2016-09-13 00:09:42 +0300

    activiti

--- app/src/main/java/com/teamrm/teamrm/Fragment/CompanyDetails.java
+++ app/src/main/java/com/teamrm/teamrm/Fragment/CompanyDetails.java
@@ -13,17 +13,17 @@ import com.teamrm.teamrm.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link CompanyDetails111111.OnFragmentInteractionListener} interface
+ * {@link CompanyDetails.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * create an instance of this fragment.
  */
-public class CompanyDetails111111 extends Fragment {
+public class CompanyDetails extends Fragment {
 
 
 
     private OnFragmentInteractionListener mListener;
 
-    public CompanyDetails111111() {}
+    public CompanyDetails() {}
 
 
 
