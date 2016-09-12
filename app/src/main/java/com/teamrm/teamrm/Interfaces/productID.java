6aa4f4ba0b01e89a5a1c4d2df16a1a83d5e23478
Shalty <shealtiel84@gmail.com>
2016-09-13 00:16:02 +0300

    activiti

--- app/src/main/java/com/teamrm/teamrm/Interfaces/productID.java
+++ app/src/main/java/com/teamrm/teamrm/Interfaces/productID.java
@@ -1,11 +1,10 @@
 package com.teamrm.teamrm.Interfaces;
 
 /**
- * Created by Administrator on 12/09/2016.
+ * Created by shalty on 12/09/2016.
  */
 public interface ProductID {
     String TICKET_ID_A00A = "A00Admin";
     String TICKET_ID_A00T = "A00Tech";
-
 
 }
