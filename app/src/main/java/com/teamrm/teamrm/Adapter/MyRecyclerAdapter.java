cbf4bb6ee06d2544cd68c718d8b64d1a8c2aeeba
Shalty <shealtiel84@gmail.com>
2016-10-24 17:18:12 +0300

    font

--- app/src/main/java/com/teamrm/teamrm/Adapter/MyRecyclerAdapter.java
+++ app/src/main/java/com/teamrm/teamrm/Adapter/MyRecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.teamrm.teamrm.Adapter;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,16 +30,22 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
     @Override
     public MyRecyclerAdapter.CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tiket_list_item, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tiket_list_pending_approval__item, null);
         CustomViewHolder viewHolder = new CustomViewHolder(view);
         return viewHolder;
        
     }
 
     @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
     public void onBindViewHolder(MyRecyclerAdapter.CustomViewHolder holder, int position) {
         Ticket Item = mTiketListItem.get(position);
-        holder.textView.setText(Item.ticketNumber);
+        holder.textView.setText(Item.userName);
+        setFont(holder);
     }
 
     @Override
@@ -54,5 +61,17 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
           
             this.textView = (TextView) view.findViewById(R.id.equipment);
         }
+    }
+    private void setFont(CustomViewHolder holder)
+    {
+       Typeface EB = Typeface.createFromAsset(mContext.getAssets(), "Assistant-ExtraBold.ttf");
+       Typeface B = Typeface.createFromAsset(mContext.getAssets(), "Assistant-Bold.ttf");
+       Typeface el = Typeface.createFromAsset(mContext.getAssets(), "Assistant-ExtraLight.ttf");
+       Typeface l = Typeface.createFromAsset(mContext.getAssets(), "Assistant-Light.ttf");
+       Typeface r = Typeface.createFromAsset(mContext.getAssets(), "Assistant-Regular.ttf");
+       Typeface SB = Typeface.createFromAsset(mContext.getAssets(), "Assistant-SemiBold.ttf");
+       
+        holder.textView.setTypeface(EB);
+        
     }
 }
