ebe623b97a97962a6fff673854877370eda9159a
Mark Farber <mark@farberz.info>
2016-11-13 22:37:09 +0200

    SplashScreen update

--- app/src/main/java/com/teamrm/teamrm/Activities/SplashScreen.java
+++ app/src/main/java/com/teamrm/teamrm/Activities/SplashScreen.java
@@ -2,6 +2,7 @@ package com.teamrm.teamrm.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,17 +13,27 @@ import com.teamrm.teamrm.R;
 public class SplashScreen extends AppCompatActivity {
 
     ImageView iconWait;
-    TextView loadingState;
+    TextView loadingStatus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splashscreen);
+        rotateWaitingIcon();
+        updateLoadingStatus("מכין אפליקציה לשימוש...");
+    }
 
-        iconWait = (ImageView)findViewById(R.id.iconWait);
+    public void rotateWaitingIcon() {
+        iconWait = (ImageView) findViewById(R.id.iconWait);
         iconWait.clearAnimation();
         Animation rotateClockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
         iconWait.startAnimation(rotateClockwise);
+    }
 
-
+    public void updateLoadingStatus(String newStatus) {
+        loadingStatus = (TextView) findViewById(R.id.loadingStatus);
+        loadingStatus.setText(newStatus);
     }
 }
+
