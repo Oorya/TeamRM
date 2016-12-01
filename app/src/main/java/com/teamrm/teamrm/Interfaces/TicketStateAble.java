57221a72bdd702e711c0a38e80690e6660d4c07b
Mark Farber <mark@farberz.info>
2016-12-01 22:46:07 +0200

    Design update TicketList logic update

--- app/src/main/java/com/teamrm/teamrm/Interfaces/TicketStateAble.java
+++ app/src/main/java/com/teamrm/teamrm/Interfaces/TicketStateAble.java
@@ -4,10 +4,10 @@ package com.teamrm.teamrm.Interfaces;
  * Created by root on 01/09/2016.
  */
 public interface TicketStateAble {
-    int TICKET_LIST_PENDIN_APPROVAL = 1;
-    int  TICKET_LIST_PENDIN_TECH_ITEM = 2;
-    int  TICKET_LIST_PENDIN_TREATMENT =3;
-    
+    int TICKET_LIST_STATUS_URGENT = 1;
+    int TICKET_LIST_STATUS_PENDING = 2;
+    int TICKET_LIST_STATUS_OK = 3;
+    int TICKET_LIST_STATUS_ERROR = 4;
     TicketStateAble getNewState();
          
 }
