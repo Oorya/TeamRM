355e7dcf85a5e473a44c7217779c19213925cf00
shealtiel <shealtiel84@gmail.com>
2016-12-06 00:39:13 +0200

    Brodcast receiver ticket alarm manager update

--- app/src/main/java/com/teamrm/teamrm/TicketStates/AdminStates/A02CNAdmin.java
+++ app/src/main/java/com/teamrm/teamrm/TicketStates/AdminStates/A02CNAdmin.java
@@ -4,6 +4,12 @@ import com.teamrm.teamrm.Interfaces.ProductID;
 import com.teamrm.teamrm.Interfaces.TicketStateAble;
 import com.teamrm.teamrm.TicketStates.TicketFactory;
 import com.teamrm.teamrm.TicketStates.TicketStateAdmin;
+import com.teamrm.teamrm.Utility.UtlAlarmManager;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static com.teamrm.teamrm.Activities.HomeScreen.context;
 
 /**
  * Created by root on 01/09/2016.
@@ -17,7 +23,12 @@ public class A02CNAdmin extends TicketStateAdmin implements TicketStateAble {
     }
     public A02CNAdmin(int ttl)
     {
-        //initials ttl example
+        Calendar cal = Calendar.getInstance(); // creates calendar
+        cal.setTime(new Date()); // sets calendar time/date
+        cal.add(Calendar.HOUR_OF_DAY, 5); // adds 5 hours
+
+        UtlAlarmManager utlAlarmManager = new UtlAlarmManager(context);
+        utlAlarmManager.setAlarm(cal.getTime());
     }
 
     @Override
