1b781c37b7600a415b4587c09ae6e54bede8711a
Mark Farber <mark@farberz.info>
2016-12-21 21:51:38 +0200

    Design update

--- app/src/main/java/com/teamrm/teamrm/Fragment/TicketView.java
+++ app/src/main/java/com/teamrm/teamrm/Fragment/TicketView.java
@@ -22,7 +22,6 @@ import com.teamrm.teamrm.Type.Ticket;
 import com.teamrm.teamrm.Type.Users;
 import com.teamrm.teamrm.Utility.UtlFirebase;
 
-import java.sql.Time;
 import java.util.List;
 
 import javax.annotation.Nullable;
@@ -37,36 +36,31 @@ public class TicketView extends Fragment implements View.OnClickListener, FireBa
     RelativeLayout userDetailOpen;
     RelativeLayout ticketDetailClose;
     RelativeLayout ticketDetailOpen;
-    TextView userName, userProfile, txtCancel,endTimeTxt;
+    TextView userName, userProfile, txtCancel, endTimeTxt;
     Ticket ticket;
     String ticketID;
     static String endTime;
-    
-    
+
+
     public TicketView() {
         // Required empty public constructor
     }
-
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle bundle = this.getArguments();
-        if (bundle != null)
-        {
-            if(bundle.getString("ticketID", "error")!="error") {
+        if (bundle != null) {
+            if (!bundle.getString("ticketID", "error").equals("error")) {
                 String ticketId = bundle.getString("ticketID", "error");
                 Log.w("TICKET_ID:  ", ticketId);
                 this.ticketID = ticketId;
-            }
-            else if (bundle.getString("time", "error")!="error")
-            {
+            } else if (!bundle.getString("time", "error").equals("error")) {
                 endTime = bundle.getString("time", "error");
             }
-            if(endTimeTxt!=null&&ticketID!=null)
-            {
-                UtlFirebase.getTicketByKey(ticketID,this);
+            if (endTimeTxt != null && ticketID != null) {
+                UtlFirebase.getTicketByKey(ticketID, this);
             }
         }
     }
@@ -75,86 +69,70 @@ public class TicketView extends Fragment implements View.OnClickListener, FireBa
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View view =  inflater.inflate(R.layout.fragment_ticket, container, false);
+        View view = inflater.inflate(R.layout.fragment_ticket, container, false);
         setListeners(view);
         Typeface REGULAR = Typeface.createFromAsset(this.getContext().getAssets(), "Assistant-Regular.ttf");
         Typeface SEMI_BOLD = Typeface.createFromAsset(this.getContext().getAssets(), "Assistant-SemiBold.ttf");
 
-        getActivity().setTitle(getContext().getResources().getString(R.string.new_ticket));
+        getActivity().setTitle(getContext().getResources().getString(R.string.ticket_new));
         getActivity().findViewById(R.id.toolbar).findViewById(R.id.toolBarItem).setVisibility(View.VISIBLE);
-        ((HomeScreen)getActivity()).btnaddTicketGon();
+        ((HomeScreen) getActivity()).btnaddTicketGon();
 
 
-        ((TextView)view.findViewById(R.id.statusTxt)).setTypeface(REGULAR);
-        ((TextView)view.findViewById(R.id.dateTimeChange)).setTypeface(REGULAR);
-        ((TextView)view.findViewById(R.id.dateTimeOpen)).setTypeface(REGULAR);
-        ((TextView)view.findViewById(R.id.ticketNum)).setTypeface(SEMI_BOLD);
+        ((TextView) view.findViewById(R.id.statusTxt)).setTypeface(REGULAR);
+        ((TextView) view.findViewById(R.id.dateTimeChange)).setTypeface(REGULAR);
+        ((TextView) view.findViewById(R.id.dateTimeOpen)).setTypeface(REGULAR);
+        ((TextView) view.findViewById(R.id.ticketNum)).setTypeface(SEMI_BOLD);
 
-        if(ticketID!=null)
+        if (ticketID != null)
 
-      // txtCancel.setText(MainActivity.userStatus.equals("User")?"בטל":"דחה");
-        if (endTime!=null)
-            endTimeTxt.setText(endTime);
+            // txtCancel.setText(MainActivity.userStatus.equals("User")?"בטל":"דחה");
+            if (endTime != null)
+                endTimeTxt.setText(endTime);
         return view;
     }
 
 
-
     @Override
     public void onClick(View view) {
-       // Toast.makeText(this.getContext(), view.getId()+"", Toast.LENGTH_SHORT).show();
-        
-        if (view.getId()==userDetailCard.getId())
-        {
+        // Toast.makeText(this.getContext(), view.getId()+"", Toast.LENGTH_SHORT).show();
+
+        if (view.getId() == userDetailCard.getId()) {
             this.getView().findViewById(R.id.userDetails).setVisibility(View.GONE);
             this.getView().findViewById(R.id.userDetailsOpen).setVisibility(View.VISIBLE);
-        }
-        else if(view.getId()==userDetailOpen.getId()) 
-        {
+        } else if (view.getId() == userDetailOpen.getId()) {
             this.getView().findViewById(R.id.userDetails).setVisibility(View.VISIBLE);
             this.getView().findViewById(R.id.userDetailsOpen).setVisibility(View.GONE);
-        }
-        else if(view.getId()== ticketDetailClose.getId())
-        {
+        } else if (view.getId() == ticketDetailClose.getId()) {
             this.getView().findViewById(R.id.ticketDetails).setVisibility(View.GONE);
             this.getView().findViewById(R.id.ticketDetailsOpen).setVisibility(View.VISIBLE);
-        }
-        else if(view.getId()== ticketDetailOpen.getId())
-        {
+        } else if (view.getId() == ticketDetailOpen.getId()) {
             this.getView().findViewById(R.id.ticketDetails).setVisibility(View.VISIBLE);
             this.getView().findViewById(R.id.ticketDetailsOpen).setVisibility(View.GONE);
-        }
-        else if(view.getId()==userProfile.getId())
-        {
-            Toast.makeText(getContext(), "USER PROFILE "+ticket.customerName, Toast.LENGTH_SHORT).show();
-        }
-        else if(view.getId()==cancel.getId())
-        {
+        } else if (view.getId() == userProfile.getId()) {
+            Toast.makeText(getContext(), "USER PROFILE " + ticket.customerName, Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == cancel.getId()) {
             UtlFirebase.changeState(ticketID,
-                    MainActivity.userStatus.equals("User")?ProductID.STATE_E00:ProductID.STATE_E01);
-        }
-        else if(view.getId()==approval.getId())
-        {
+                    MainActivity.userStatus.equals("User") ? ProductID.STATE_E00 : ProductID.STATE_E01);
+        } else if (view.getId() == approval.getId()) {
             //UtlFirebase.stateListener("Admin",MainActivity.email,"NULL");
-        }
-        else if(view.getId()==endTimeTxt.getId())
-        {
-            ((HomeScreen)getActivity()).onDrawerItemSelected(view,2);
-            ((HomeScreen)getActivity()).setTitle("יומן");
+        } else if (view.getId() == endTimeTxt.getId()) {
+            ((HomeScreen) getActivity()).onDrawerItemSelected(view, 2);
+            ((HomeScreen) getActivity()).setTitle("יומן");
         }
     }
-    private void setListeners(View view)
-    {
-        userDetailCard = (CardView)view.findViewById(R.id.userDetailCard);
+
+    private void setListeners(View view) {
+        userDetailCard = (CardView) view.findViewById(R.id.userDetailCard);
         userDetailOpen = (RelativeLayout) view.findViewById(R.id.name);
-        ticketDetailClose = (RelativeLayout)view.findViewById(R.id.ticketHeading);
+        ticketDetailClose = (RelativeLayout) view.findViewById(R.id.ticketHeading);
         ticketDetailOpen = (RelativeLayout) view.findViewById(R.id.ticketHeadingExpand);
-        userName = (TextView)view.findViewById(R.id.userNameCard);
-        userProfile = (TextView)view.findViewById(R.id.userProfile);
-        approval = (CardView)view.findViewById(R.id.ok);
-        cancel = (CardView)view.findViewById(R.id.cancel);
-        txtCancel = (TextView)view.findViewById(R.id.txtCancel);
-        endTimeTxt = (TextView)view.findViewById(R.id.dateTimeChange);
+        userName = (TextView) view.findViewById(R.id.userNameCard);
+        userProfile = (TextView) view.findViewById(R.id.userProfileLabel);
+        approval = (CardView) view.findViewById(R.id.ok);
+        cancel = (CardView) view.findViewById(R.id.cancel);
+        txtCancel = (TextView) view.findViewById(R.id.txtCancel);
+        endTimeTxt = (TextView) view.findViewById(R.id.dateTimeChange);
         endTimeTxt.setOnClickListener(this);
         userDetailCard.setOnClickListener(this);
         userDetailOpen.setOnClickListener(this);
@@ -167,13 +145,12 @@ public class TicketView extends Fragment implements View.OnClickListener, FireBa
 
     @Override
     public void resultTicket(Ticket ticket) {
-        this.ticket=ticket;
+        this.ticket = ticket;
         userName.setText(ticket.customerName);
 
-        if(ticket.state != ProductID.STATE_A03)
-        {
-        UtlFirebase.changeState(ticket.ticketId, ProductID.STATE_A03);
-        ticket.state = ProductID.STATE_A03;
+        if (ticket.state != ProductID.STATE_A03) {
+            UtlFirebase.changeState(ticket.ticketId, ProductID.STATE_A03);
+            ticket.state = ProductID.STATE_A03;
         }
     }
 
