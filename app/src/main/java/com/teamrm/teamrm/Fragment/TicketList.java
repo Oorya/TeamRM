7e0416812b95ec081b7878bb126606ea953a0c31
shealtiel <shealtiel84@gmail.com>
2017-02-07 03:32:55 +0200

    calendar

--- app/src/main/java/com/teamrm/teamrm/Fragment/TicketList.java
+++ app/src/main/java/com/teamrm/teamrm/Fragment/TicketList.java
@@ -1,12 +1,16 @@
 package com.teamrm.teamrm.Fragment;
 
 
+import android.content.DialogInterface;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -14,11 +18,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.BounceInterpolator;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.GravityEnum;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.teamrm.teamrm.Adapter.TicketListAdapter;
 import com.teamrm.teamrm.Interfaces.FireBaseAble;
 import com.teamrm.teamrm.Interfaces.FragmentHelper;
+import com.teamrm.teamrm.Interfaces.TicketStatus;
 import com.teamrm.teamrm.R;
 import com.teamrm.teamrm.Type.Category;
 import com.teamrm.teamrm.Type.Company;
@@ -30,12 +39,14 @@ import com.teamrm.teamrm.Type.Users;
 import com.teamrm.teamrm.Utility.UtlFirebase;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TicketList extends Fragment implements FireBaseAble {
+public class TicketList extends Fragment implements FireBaseAble,View.OnClickListener{
 
     public RecyclerView mRecyclerView;
     private List<TicketLite> ticketLiteList = new ArrayList<>();
@@ -43,6 +54,7 @@ public class TicketList extends Fragment implements FireBaseAble {
     private TextView title, filter, search, order;
     private SwipeRefreshLayout swipeContainer;
     FloatingActionButton floatBtn;
+    private AlertDialog chekDialog;
 
     public TicketList() {
     }
@@ -60,6 +72,7 @@ public class TicketList extends Fragment implements FireBaseAble {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              final Bundle savedInstanceState) {
@@ -67,10 +80,10 @@ public class TicketList extends Fragment implements FireBaseAble {
 
         Typeface SEMI_BOLD = Typeface.createFromAsset(this.getContext().getAssets(), "Assistant-SemiBold.ttf");
 
-        filter = (TextView) view.findViewById(R.id.filtertxt);
+        LinearLayout filter = (LinearLayout) view.findViewById(R.id.filter);
         search = (TextView) view.findViewById(R.id.searchTxt);
         order = (TextView) view.findViewById(R.id.sortimgTxt);
-
+        filter.setOnClickListener(this);
 
         //((TextView)view.findViewById(R.id.titleText)).setTypeface(BOLD);
         ((TextView) view.findViewById(R.id.filtertxt)).setTypeface(SEMI_BOLD);
@@ -84,6 +97,7 @@ public class TicketList extends Fragment implements FireBaseAble {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.RecyclerView);
         mRecyclerView.setLayoutManager(recyclerViewManager);
 
+        orderList();
         ticketListAdapter = new TicketListAdapter(getContext(), ticketLiteList);
         mRecyclerView.setAdapter(ticketListAdapter);
 
@@ -130,7 +144,82 @@ public class TicketList extends Fragment implements FireBaseAble {
         return view;
     }
 
-       @Override
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.filter:
+            {
+
+                final CharSequence[] items = {TicketStatus.pendingOrderedTech,
+                        TicketStatus.ticketApproval,
+                        TicketStatus.ticketCanceled,
+                        TicketStatus.ticketClosed,
+                        TicketStatus.timeIsOver,
+                        TicketStatus.waitForApproval};
+                final ArrayList<String> selectedItems=new ArrayList();
+
+                new MaterialDialog.Builder(getContext())
+                        .title(R.string.label_ticket_filter)
+                        .items(items)
+                        .itemsCallbackMultiChoice(null, new MaterialDialog.ListCallbackMultiChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, Integer[] which, CharSequence[] text) {
+
+                                for (Integer item : which)
+                                {
+                                    selectedItems.add((String)items[item]);
+                                    Log.d("item", items[item]+"");
+                                    Log.d("item", selectedItems.get(selectedItems.size()-1)+"");
+                                    orderList();
+                                  ticketListAdapter.notifyDataSetChanged();
+
+
+                                }
+                                return true;
+                            }
+                        })
+                        .positiveText("OK")
+                        .contentColorRes(R.color.textColor_primary)
+                        .contentGravity(GravityEnum.CENTER)
+                        .negativeText(R.string.label_button_cancel)
+                        .titleGravity(GravityEnum.CENTER)
+                        .buttonsGravity(GravityEnum.END)
+                        .backgroundColorRes(R.color.app_bg)
+                        .titleColorRes(R.color.textColor_lighter)
+                        .positiveColorRes(R.color.colorPrimary)
+                        .negativeColorRes(R.color.colorPrimaryDark)
+                        .dividerColorRes(R.color.textColor_lighter)
+                        .show();
+                break;
+            }
+            case R.id.search:
+            {
+                break;
+            }
+            case R.id.sort:
+            {
+                break;
+            }
+        }
+    }
+    private void orderList()
+    {
+        Collections.sort(ticketLiteList, new Comparator<TicketLite>() {
+            @Override
+            public int compare(TicketLite o1, TicketLite o2) {
+                Integer iInteger1 = new Integer(o1.getTicketPresentation());
+                Integer iInteger2 = new Integer(o2.getTicketPresentation());
+
+
+                return iInteger1.compareTo(iInteger2);
+            }
+        });
+
+
+    }
+
+    @Override
     public void resultTicket(Ticket ticket) {
 
     }
@@ -147,7 +236,7 @@ public class TicketList extends Fragment implements FireBaseAble {
 
     @Override
     public void ticketLiteListCallback(List<TicketLite> ticketLites) {
-        ticketLiteList = ticketLites;
+
 
     }
 
